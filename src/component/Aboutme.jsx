995d812774d6fd1cde2c5eb81f2f5c503f0b12dd
kannan.j <113454965+kannan304@users.noreply.github.com>
2024-06-22 19:36:50 +0530

    Add files via upload

---
+++ src/component/Aboutme.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from "./About.module.css"
+
+
+const Aboutme = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default Aboutme
