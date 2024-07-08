56c4234f6f56bc9bbe8f53fad82650449b092792
KANNAN.J <113454965+kannan304@users.noreply.github.com>
2024-07-08 11:39:08 +0530

    Add files via upload

---
+++ src/component/Project.jsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import style from "./Project.module.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+const projects = [
+  {
+    id: 1,
+    name: "e-commerce",
+    screenshot: require("./Media/e-com.jpg")
+  },
+  {
+    id: 2,
+    name: "todo list",
+    screenshot: require("./Media/an-icon-of-todo-list1.jpg")
+  },
+  {
+    id: 3,
+    name: "Chat bot",
+    screenshot: require("./Media/chatbot-chat-message.png")
+  },
+  {
+    id: 4,
+    name: "Empolyee Management System",
+    screenshot: require("./Media/emplogo.jpg")
+  },
+  
+];
+
+const Project = () => {
+  const [hover, setHover] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const handleMouseEnter = (project) => {
+    setHover(true);
+    setSelectedProject(project);
+  };
+
+  const handleMouseLeave = () => {
+    setHover(false);
+  };
+
+  return (
+
+    
+    <div className={style.content}>
+      <h2>My Projects</h2>
+      <section>
+        {projects.map((project) => (
+          <div key={project.id} className={style.contentProject}>
+            <div className={style.imageHover}onMouseEnter={() => handleMouseEnter(project)} onMouseLeave={handleMouseLeave}>
+             
+              <img src={project.screenshot} alt="Image" className={style.hoverImage} />
+              {hover && selectedProject === project && (
+                <div
+                  className={style.hoverText} 
+                  style={{
+                    backgroundColor: "rgb(250 232 255)",
+                    transition: "background-color 0.3s ease-in-out",
+                    
+                  }}
+                >
+                  <p data-aos="slide-up" data-aos-once="false">{project.name}</p>
+                </div>
+              )}
+            </div>
+          </div>
+        ))}
+      </section>
+    </div>
+  );
+};
+
+export default Project;
